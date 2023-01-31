ebf95866709e4f273f5385154befbfa0d92f9aa6
João <joaovictorpfr@gmail.com>
2023-01-31 11:34:40 -0300

    Base code example in Numpy lib

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
+# Importação padrão do Numpy
 import Numpy as np;
+
+# Usada a função de busca do arquivo
 
 km = np.loadtxt('./Numpy/data/carros-km.txt')
 anos = np.loadtxt('./Numpy/data/carros-anos.txt', dtype = int)
 
+# Operação de quilometragem média e exibição no console:
 km_media = km / (2022 - anos)
 print(km_media)
