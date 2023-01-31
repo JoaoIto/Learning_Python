9e9c2f0b86d8d457666c28a6dd8cac6895e7c871
João <joaovictorpfr@gmail.com>
2023-01-31 11:47:50 -0300

    Fix errors from lib Numpy

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 # Importação padrão do Numpy
-import Numpy as np;
+import numpy as np
 
 # Usada a função de busca do arquivo
 
-km = np.loadtxt('./Numpy/data/carros-km.txt')
-anos = np.loadtxt('./Numpy/data/carros-anos.txt', dtype = int)
+km = np.loadtxt('./data/carros-km.txt')
+anos = np.loadtxt('./data/carros-anos.txt', dtype = int)
 
 # Operação de quilometragem média e exibição no console:
 km_media = km / (2022 - anos)
