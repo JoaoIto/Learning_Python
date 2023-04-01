7caf3f3bb3c1c7f020edcb35a01d978a4e895b9a
João <joaovictorpfr@gmail.com>
2023-03-31 23:40:02 -0300

    Add code media notes school

---
+++ Base/Estruturas - Rep_Cond/index.py
@@ -0,0 +1,14 @@
+nota_1 = float(input('Digite a 1° nota: '))
+nota_2 = float(input('Digite a 2° nota: '))
+
+print(f'Média: {(nota_1+nota_2)/2}')
+
+nota_1 = float(input('Digite a 1° nota: '))
+nota_2 = float(input('Digite a 2° nota: '))
+
+print(f'Média: {(nota_1+nota_2)/2}')
+
+nota_1 = float(input('Digite a 1° nota: '))
+nota_2 = float(input('Digite a 2° nota: '))
+
+print(f'Média: {(nota_1+nota_2)/2}')
